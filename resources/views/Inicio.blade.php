5fc5beb9d726d037da80ea4e00135c08dc478479
Carlos José Mantilla Cote <140472760+CarlosMantillaC@users.noreply.github.com>
2025-03-03 18:22:51 -0500

    add css/js app.js

--- resources/views/Inicio.blade.php
+++ resources/views/Inicio.blade.php
@@ -1,51 +1,266 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Corpusfit - Login</title>
-    <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-</head>
-<body>
-    <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
-        <div class="container">
-            <div class="card login-card">
-                <div class="row no-gutters">
-                    <div class="col-md-5">
-                        <img src="img/gallery/gallery-4.jpg" alt="login" class="login-card-img">
-                    </div>
-                    <div class="col-md-7">
-                        <div class="card-body">
-                            <div class="brand-wrapper">
-                                <img src="img/logo.png" alt="logo" class="logo">
-                            </div>
-                            <p class="login-card-description">Inicia sesión en tu cuenta</p>
-                            
-                            <form action="LoginServlet" method="post">
-                                <div class="form-group">
-                                    <label for="username" class="sr-only">Usuario</label>
-                                    <input type="email" name="username" id="username" class="form-control" placeholder="Correo electrónico">
-                                </div>
-                                <div class="form-group mb-4">
-                                    <label for="password" class="sr-only">Contraseña</label>
-                                    <input type="password" name="password" id="password" class="form-control" placeholder="Contraseña">
-                                </div>
-                                <input name="login" id="login" class="btn btn-block login-btn mb-4" type="submit" value="Iniciar sesión">
-                                
-                                <!-- Botón para regresar al index -->
-                                <a href="index.html" class="btn btn-black btn-block">Regresar al Inicio</a>
-                            </form>
-                            <br>
-                            <p class="login-card-footer-text">¿No estás registrado? <br> Comunícate con la recepción del gimnasio.</p>
+@extends('app')
+
+@section('content')
+<!-- Hero Section Begin-->
+<section class="hero-section">
+    <div class="hs-slider owl-carousel">
+        <div class="hs-item set-bg" data-setbg="img/hero/hero-1.jpg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 offset-lg-6">
+                        <div class="hi-text">
+                            <span>Pastorea tu cuerpo</span>
+                            <h1>Se <strong>fuerte</strong> entrenando duro</h1>
+                            <a href="contact.jsp" class="primary-btn">Obtener información</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </main>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-</body>
-</html>
+        <div class="hs-item set-bg" data-setbg="img/hero/hero-2.jpg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 offset-lg-6">
+                        <div class="hi-text">
+                            <span>Pastorea tu cuerpo</span>
+                            <h1>Se <strong>fuerte</strong> entrenando duro</h1>
+                            <a href="contact.jsp" class="primary-btn">Obtener información</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Hero Section End -->
+
+<!-- ChoseUs Section Begin -->
+<section class="choseus-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-title">
+                    <span>¿Por qué elegirnos?</span>
+                    <h2>SUPERA TUS LÍMITES</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+                <div class="cs-item">
+                    <span class="flaticon-034-stationary-bike"></span>
+                    <h4>Equipo moderno</h4>
+                    <p>Es esencial que el cliente valore un equipo de gimnasio moderno, que permite entrenar de
+                        forma efectiva y segura, optimizando resultados y reduciendo riesgos</p>
+                </div>s
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="cs-item">
+                    <span class="flaticon-002-dumbell"></span>
+                    <h4>Plan de entrenamiento</h4>
+                    <p>Un plan de entrenamiento profesional está diseñado para optimizar el rendimiento, adaptándose
+                        a las metas individuales y asegurando progresos constantes de manera segura y efectiva.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="cs-item">
+                    <span class="flaticon-014-heart-beat"></span>
+                    <h4>Único para tus necesidades</h4>
+                    <p>Adaptado exclusivamente a tus necesidades, brindando soluciones personalizadas para alcanzar
+                        tus objetivos de manera efectiva y única.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- ChoseUs Section End -->
+
+<!-- Classes Section Begin -->
+<section class="classes-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-title">
+                    <span>Nuestras clases</span>
+                    <h2>LO QUE PODEMOS OFRECER</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="class-item">
+                    <div class="ci-pic">
+                        <img src="img/classes/class-1.jpg" alt="">
+                    </div>
+                    <div class="ci-text">
+                        <span>Deportes de resistencia</span>
+                        <h5>correr, ciclismo</h5>
+                        <a href="login.jsp"><i class="fa fa-angle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="class-item">
+                    <div class="ci-pic">
+                        <img src="img/classes/class-2.jpg" alt="">
+                    </div>
+                    <div class="ci-text">
+                        <span>Actividades de fuerza</span>
+                        <h5>levantamiento de pesas, <br>crossfit</h5>
+                        <a href="login.jsp"><i class="fa fa-angle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="class-item">
+                    <div class="ci-pic">
+                        <img src="img/classes/class-3.jpg" alt="">
+                    </div>
+                    <div class="ci-text">
+                        <span>Actividades de flexibilidad y <br>equilibrio</span>
+                        <h5>yoga, pilates</h5>
+                        <a href="login.jsp"><i class="fa fa-angle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="class-item">
+                    <div class="ci-pic">
+                        <img src="img/classes/class-4.jpg" alt="">
+                    </div>
+                    <div class="ci-text">
+                        <span>Deportes de combate</span>
+                        <h4>boxeo, karate</h4>
+                        <a href="login.jsp"><i class="fa fa-angle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="class-item">
+                    <div class="ci-pic">
+                        <img src="img/classes/class-5.jpg" alt="">
+                    </div>
+                    <div class="ci-text">
+                        <span>Actividades recreativas</span>
+                        <h4>baile, zumba</h4>
+                        <a href="login.jsp"><i class="fa fa-angle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ChoseUs Section End -->
+
+<!-- Banner Section Begin -->
+<section class="banner-section set-bg" data-setbg="img/banner-bg.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="bs-text">
+                    <h2>Contactanos para obtener mas ofertas</h2>
+                    <div class="bt-tips">Donde la salud, la belleza y el fitness se encuentran.</div>
+                    <a href="contact.jsp" class="primary-btn  btn-normal">Obtener información</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Banner Section End -->
+
+<!-- Gallery Section Begin -->
+<div class="gallery-section">
+    <div class="gallery">
+        <div class="grid-sizer"></div>
+        <div class="gs-item grid-wide set-bg" data-setbg="img/gallery/gallery-9.jpg">
+            <a href="img/gallery/gallery-9.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
+        </div>
+        <div class="gs-item set-bg" data-setbg="img/gallery/gallery-2.jpg">
+            <a href="img/gallery/gallery-2.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
+        </div>
+        <div class="gs-item set-bg" data-setbg="img/gallery/gallery-3.jpg">
+            <a href="img/gallery/gallery-3.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
+        </div>
+        <div class="gs-item set-bg" data-setbg="img/gallery/gallery-4.jpg">
+            <a href="img/gallery/gallery-4.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
+        </div>
+        <div class="gs-item set-bg" data-setbg="img/gallery/gallery-5.jpg">
+            <a href="img/gallery/gallery-5.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
+        </div>
+        <div class="gs-item grid-wide set-bg" data-setbg="img/gallery/gallery-6.jpg">
+            <a href="img/gallery/gallery-6.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
+        </div>
+    </div>
+</div>
+<!-- Gallery Section End -->
+
+<!-- Team Section Begin -->
+<section class="team-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="team-title">
+                    <div class="section-title">
+                        <span>Nuestro equipo</span>
+                        <h2>FORMATE CON EXPERTOS</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="ts-slider owl-carousel">
+                <div class="col-lg-4">
+                    <div class="ts-item set-bg" data-setbg="img/team/team-1.jpg">
+                        <div class="ts_text">
+                            <h4>Selena Aguilar</h4>
+                            <span>Entrenadora</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ts-item set-bg" data-setbg="img/team/team-2.jpg">
+                        <div class="ts_text">
+                            <h4>Oliver Johnson</h4>
+                            <span>Entrenador</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ts-item set-bg" data-setbg="img/team/team-3.jpg">
+                        <div class="ts_text">
+                            <h4>Daniel Smith</h4>
+                            <span>Entrenador</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ts-item set-bg" data-setbg="img/team/team-4.jpg">
+                        <div class="ts_text">
+                            <h4>Noah Williams</h4>
+                            <span>Entrenador</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ts-item set-bg" data-setbg="img/team/team-5.jpg">
+                        <div class="ts_text">
+                            <h4>Daniela Acevedo</h4>
+                            <span>Entrenadora</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ts-item set-bg" data-setbg="img/team/team-6.jpg">
+                        <div class="ts_text">
+                            <h4>Camila Herrera</h4>
+                            <span>Entrenadora</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Team Section End -->
+@endsection
---
+++ resources/views/app.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Gym Template">
+    <meta name="keywords" content="Gym, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="${pageContext.request.contextPath}/img/Logoicono.ico" type="image/x-icon">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&display=swap" rel="stylesheet">
+    <!-- Css Styles -->
+
+@if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+@endif
+    
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Offcanvas Menu Section Begin -->
+    <div class="offcanvas-menu-overlay"></div>
+    <div class="offcanvas-menu-wrapper">
+        <div class="canvas-close">
+            <i class="fa fa-close"></i>
+        </div>
+        <div class="canvas-search search-switch">
+            <i class="fa fa-search"></i>
+        </div>
+        <nav class="canvas-menu mobile-menu">
+            <ul>
+                <li><a href="./index.jsp">Inicio</a></li>
+                <li><a href="./about-us.jsp">Sobre nosotros</a></li>
+                <li><a href="./services.jsp">Servicios</a></li>
+                <li><a href="./team.jsp">Nuestro equipo</a></li>
+                <li><a href="./contact.jsp">Contáctanos</a></li>
+                <li><a href="./login.jsp">Soy Corpusfit</a></li>
+            </ul>
+        </nav>
+        <div id="mobile-menu-wrap"></div>
+
+    </div>
+    <!-- Offcanvas Menu Section End -->
+
+    <!-- Header Section Begin -->
+    <header class="header-section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="logo">
+                        <a href="./index.jsp">
+                            <img src="img/logo.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <nav class="nav-menu">
+                        <ul>
+                            <li><a href="./index.jsp">Inicio</a></li>
+                            <li><a href="./about-us.jsp">Sobre nosotros</a></li>
+                            <li><a href="./services.jsp">Servicios</a></li>
+                            <li><a href="./team.jsp">Nuestro equipo</a></li>
+                            <li><a href="./contact.jsp">Contáctanos</a></li>
+                            <li><a href="./login.jsp">Soy Corpusfit</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="canvas-open">
+                <i class="fa fa-bars"></i>
+            </div>
+        </div>
+    </header>
+    <!-- Header End -->
+    @yield('content')
+</body>
+
+</html>
