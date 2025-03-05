135b4880125d5f8c20e513b4cd5fe65a2e78e56a
AcerosDavid <142636565+AcerosDavid@users.noreply.github.com>
2025-03-05 23:07:03 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('inicio', function () {
     return view('inicio');
 });
-
-Route::get('Login', function () {
-    return view('Login');
+Route::get('inicio2', function () {
+    return view('inicio2');
 });
