bed8f97483646726625ed5295466fad758ed1e9f
Carlos José Mantilla Cote <140472760+CarlosMantillaC@users.noreply.github.com>
2025-04-11 08:41:23 -0500

    add tailwind

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+
+    @vite([
+    
+    'resources/css/tailwind.css'])
 </head>
+
 <body>
     <h1>Bienvenido al Dashboard</h1>
+    <h1 class="text-3xl text-center text-red-600">
+        Hello world!
+    </h1>
+    
     <form action="{{ route('logout') }}" method="POST">
-    @csrf
-    <button type="submit">Cerrar sesión</button>
-</form>
+        @csrf
+        <button type="submit">Cerrar sesión</button>
+    </form>
 
 </body>
+
 </html>
