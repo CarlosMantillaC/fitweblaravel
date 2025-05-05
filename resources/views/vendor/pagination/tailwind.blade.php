357652be5f4128c9f045fa221a8cb88c6a089dca
AcerosDavid <142636565+AcerosDavid@users.noreply.github.com>
2025-05-04 20:54:25 -0500

    Update Responsive list space-1

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -1,38 +1,38 @@
 @if ($paginator->hasPages())
     <nav role="navigation" class="flex items-center justify-between" aria-label="Pagination Navigation">
         <div class="flex justify-center flex-1">
-        <div class="inline-flex space-x-1 rounded-xl bg-[#0A0A0A] px-4 py-2 border border-transparent">
-
-                
+            <div class="inline-flex space-x-1 rounded-xl bg-[#0A0A0A] px-4 py-2 border border-transparent">
                 {{-- Previous Page Link --}}
                 @if ($paginator->onFirstPage())
                     <span class="px-5 py-2 text-sm text-gray-500 bg-[#151515] rounded">Anterior</span>
                 @else
-                    <a href="{{ $paginator->previousPageUrl() }}" class="px-3 py-1 text-sm text-white bg-[#151515] hover:bg-[#f36100] rounded">Anterior</a>
+                    <a href="{{ $paginator->previousPageUrl() }}" class="px-5 py-2 text-sm text-white bg-[#151515] hover:bg-[#f36100] rounded">Anterior</a>
                 @endif
 
-                {{-- Pagination Elements --}}
-                @foreach ($elements as $element)
-                    @if (is_string($element))
-                        <span class="px-5 py-2 text-sm text-gray-500 rounded">{{ $element }}</span>
-                    @endif
+                {{-- Pagination Elements (Hidden on Mobile) --}}
+                <div class="hidden sm:flex sm:space-x-1">
+                    @foreach ($elements as $element)
+                        @if (is_string($element))
+                            <span class="px-5 py-2 text-sm text-gray-500 rounded">{{ $element }}</span>
+                        @endif
 
-                    @if (is_array($element))
-                        @foreach ($element as $page => $url)
-                            @if ($page == $paginator->currentPage())
-                                <span class="px-5 py-2 text-sm font-semibold text-white bg-[#f36100] rounded">{{ $page }}</span>
-                            @else
-                                <a href="{{ $url }}" class="px-5 py-2 text-sm text-white bg-[#151515] hover:bg-[#f36100] rounded transition-all">{{ $page }}</a>
-                            @endif
-                        @endforeach
-                    @endif
-                @endforeach
+                        @if (is_array($element))
+                            @foreach ($element as $page => $url)
+                                @if ($page == $paginator->currentPage())
+                                    <span class="px-5 py-2 text-sm font-semibold text-white bg-[#f36100] rounded">{{ $page }}</span>
+                                @else
+                                    <a href="{{ $url }}" class="px-5 py-2 text-sm text-white bg-[#151515] hover:bg-[#f36100] rounded transition-all">{{ $page }}</a>
+                                @endif
+                            @endforeach
+                        @endif
+                    @endforeach
+                </div>
 
                 {{-- Next Page Link --}}
                 @if ($paginator->hasMorePages())
                     <a href="{{ $paginator->nextPageUrl() }}" class="px-5 py-2 text-sm text-white bg-[#151515] hover:bg-[#f36100] rounded">Siguiente</a>
                 @else
-                    <span class="px-3 py-1 text-sm text-gray-500 bg-[#151515] rounded">Siguiente</span>
+                    <span class="px-5 py-2 text-sm text-gray-500 bg-[#151515] rounded">Siguiente</span>
                 @endif
             </div>
         </div>
